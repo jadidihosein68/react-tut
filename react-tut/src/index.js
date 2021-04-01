c5d5bffea7b29e44cc12430ec3a7bb82ad0102a3
Mohammad hossein jadidi <jadidi.hosein@gmail.com>
2021-04-01 11:34:16 +0800

    initial study

---
+++ react-tut/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const element = <h1>hellow world </h1>
+
+
+console.log(element);
+
+
+ReactDOM.render(element,document.getElementById('root')); // get the reference to 'root' and render the element inside the root
