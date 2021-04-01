7ca0d752bdfead5c7693d96a3307ef270ed95ac6
Mohammad hossein jadidi <jadidi.hosein@gmail.com>
2021-04-01 14:47:56 +0800

    done 3-3

--- react-tut/src/index.js
+++ react-tut/src/index.js
@@ -4,3 +4,18 @@ import {Teacher} from "./teacher";
 const tacher = new Teacher("kambiz" ,"fucker");
 tacher.teach();
 */
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
+import registerServiceWorker from "./registerServiceWorker";
+
+import Counter from "./components/counter"
+
+
+
+ReactDOM.render(<Counter/>, document.getElementById('root'));
+registerServiceWorker();
